8227a2c0eef186629fa31e7fb7c1f5e3ad4b4676
Riley O'Shea <osheari1@gmail.com>
2024-08-11 09:03:43 -0400

    feat: update org settings for gptel

--- configs/gptel.el
+++ configs/gptel.el
@@ -2,8 +2,10 @@
 
 (use-package! gptel
   :config
-  (setq! gptel-api-key (get-authinfo-entry "api.openai.com"))
-  (setq! gptel-model "gpt-4o"))
+  (setq! gptel-api-key (get-authinfo-entry "api.openai.com")
+         gptel-model "gpt-4o"
+         gptel-default-mode 'org-mode))
+
 
 (map! :leader
       :desc "gptel"
@@ -18,4 +20,9 @@
         (programming . "You are a large language model and a careful programmer. Provide code and only code as output without any additional text, prompt or note.")
         (emacs . "Act as an emacs expert. Respond concisely. Where examples would support the answer, please add them.")
         (elisp . "You are an expert emacs lisp programmer. Provide code and only code as output without any additional text, prompt or note.")
-        ))
+        )
+      gptel-prompt-prefix-alist
+      '((markdown-mode . "# ")
+        (org-mode . "* ")
+        (text-mode . "# "))
+      )
