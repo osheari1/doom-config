53070e44dfb4946452deaff5e59a07345dc0a9d9
Riley O'Shea <osheari1@gmail.com>
2024-08-19 16:56:22 -0400

    feat: add capture templates and immediate node insertion

--- confs/roam.el
+++ confs/roam.el
@@ -18,7 +18,29 @@
                    "%<%Y%m%d%H%M%S>-turtles-${slug}.org"
                    "#+title: Turtles-${title}\n#+filetags: TEST_FILE_TAG\n#+date: %U\n"
                    )
-          :unnarrowed t)
+          :unnarrowed t))
+
+       ;; capture templates dailies
+       org-roam-dailies-capture-templates
+       '(("d" "default" entry "* %<%H:%M>: %?" :target
+          (file+head "%<%Y-%m-%d>.org" "#+title: %<%Y-%m-%d>\n"))))
 
 
-         ))
+;; Like org-roam-node-insert, but won't prompt for content.
+;; Use when you want to create a new node from another node without
+;; filling in content.
+(defun org-roam-node-insert-immediate (arg &rest args)
+  (interactive "P")
+  (let ((args (cons arg args))
+        (org-roam-capture-templates (list (append (car org-roam-capture-templates)
+                                                  '(:immediate-finish t)))))
+    (apply #'org-roam-node-insert args)))
+
+
+;; org-mode map
+(map! (:when (modulep! :lang org +roam2)
+        :map org-mode-map
+        :localleader
+        :prefix ("m" . "org-roam")
+        "G" #'org-id-get-create
+        "I" #'org-roam-node-insert-immediate))
