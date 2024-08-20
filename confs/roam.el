4ba7dff21a7e5c86640608b794ac4e5e6d534900
Riley O'Shea <osheari1@gmail.com>
2024-08-19 20:02:29 -0400

    feat: add custom org-roam project capture

--- confs/roam.el
+++ confs/roam.el
@@ -1,41 +1,41 @@
 ;;; confs/roam.el -*- lexical-binding: t; -*-
 
+
 (setq! org-roam-directory "~/org/"
        org-roam-graph-viewer "/usr/bin/emacs"
 
        ;; In org files, don't need to type '[[]]' to get completion session
        org-roam-completion-everywhere t
 
+
        ;; capture templates
+       ;; Use the below for example templates
+       ;;
+       ;; #+begin_src emacs-lisp
+       ;; ("e" "example" plain
+       ;;  (file "~/.config/doom/templates/org-roam/example.org")
+       ;;  :target (file+head
+       ;;           "%<%Y%m%d%H%M%S>-turtles-${slug}.org"
+       ;;           "#+title: Turtles-${title}\n#+filetags: TEST_FILE_TAG\n#+date: %U\n#+category: ${title}\n"
+       ;;           )
+       ;;  :unnarrowed t)
+       ;; #+end_src
        org-roam-capture-templates
-       '(
-         ("d" "default" plain "%?"
-          :target (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n")
-          :unnarrowed t)
-         ("e" "example" plain
-          (file "~/.config/doom/templates/org-roam/example.org")
-          :target (file+head
-                   "%<%Y%m%d%H%M%S>-turtles-${slug}.org"
-                   "#+title: Turtles-${title}\n#+filetags: TEST_FILE_TAG\n#+date: %U\n"
-                   )
+       '(("d" "default" plain "%?"
+          :target (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n#+category: ${title}")
           :unnarrowed t))
 
        ;; capture templates dailies
        org-roam-dailies-capture-templates
-       '(("d" "default" entry "* %<%H:%M>: %?" :target
+       '(("d" "default" entry "* %?" :target
           (file+head "%<%Y-%m-%d>.org" "#+title: %<%Y-%m-%d>\n"))))
 
-
-;; Like org-roam-node-insert, but won't prompt for content.
-;; Use when you want to create a new node from another node without
-;; filling in content.
-(defun org-roam-node-insert-immediate (arg &rest args)
-  (interactive "P")
-  (let ((args (cons arg args))
-        (org-roam-capture-templates (list (append (car org-roam-capture-templates)
-                                                  '(:immediate-finish t)))))
-    (apply #'org-roam-node-insert args)))
-
+;; org-roam notes map
+(map!
+ (:prefix-map ("n" . "notes")
+              (:when (modulep! :lang org +roam2)
+                (:prefix ("r" . "roam")
+                 :desc "Find project" "p" #'my/org-roam-find-project))))
 
 ;; org-mode map
 (map! (:when (modulep! :lang org +roam2)
@@ -43,4 +43,53 @@
         :localleader
         :prefix ("m" . "org-roam")
         "G" #'org-id-get-create
-        "I" #'org-roam-node-insert-immediate))
+        "I" #'my/org-roam-node-insert-immediate))
+
+
+
+
+;; Updates org-agenda-files with current buffer.
+;; Used for custom capture workflows
+(defun my/org-roam-project-finalize-hook ()
+  "Adds the captured project file to `org-agenda-files'
+if the capture was not aborted."
+  ;; Remove the hook since it was added temporarily
+  (remove-hook 'org-capture-after-finalize-hook #'my/org-roam-project-finalize-hook)
+
+  ;; Add project file to the agenda list if the capture was confirmed
+  (unless org-note-abort
+    (with-current-buffer (org-capture-get :buffer)
+      (add-to-list 'org-agenda-files (buffer-file-name)))))
+
+
+;; Searching all nodes for those with a filetag == project
+(defun my/org-roam-find-project ()
+  (interactive)
+  ;; Add the project file to the agenda after capture is finished
+  (add-hook 'org-capture-after-finalize-hook #'my/org-roam-project-finalize-hook)
+
+  ;; Select a project file to open, creating it if necessary
+  (org-roam-node-find
+   nil
+   nil
+   (my/org-roam-filter-by-tag "project")
+   nil
+   :templates '(("p" "project" plain
+                 (file "~/.config/doom/templates/org-roam/project.org")
+                 :target (file+head
+                          "%<%Y%m%d%H%M%S>-${slug}.org"
+                          "#+title: ${title}\n#+category: ${title}\n#+filetags: project")
+                 :unnarrowed t))))
+
+
+
+;; Like org-roam-node-insert, but won't prompt for content.
+;; Use when you want to create a new node from another node without
+;; filling in content.
+(defun my/org-roam-node-insert-immediate (arg &rest args)
+  (interactive "P")
+  (let ((args (cons arg args))
+        (org-roam-capture-templates (list (append (car org-roam-capture-templates)
+                                                  '(:immediate-finish t)))))
+    (apply #'org-roam-node-insert args)))
+
