489ada42fe7ba203dfcf2bd8bb1ff40f709ad152
Riley O'Shea <osheari1@gmail.com>
2024-08-20 14:50:53 -0400

    feat: add tagging system and log completion timestamp

--- confs/org-mode.el
+++ confs/org-mode.el
@@ -1,4 +1,41 @@
 ;;; confs/org.el -*- lexical-binding: t; -*-
 
 
-(setq org-agenda-files (directory-files-recursively org-directory "\\.org$"))
+(setq org-agenda-files (directory-files-recursively org-directory "\\.org$")
+      org-log-done 'time    ;; Adds a timestamp and/or note upon task completion
+      )
+
+
+
+(setq org-tag-alist
+      '(
+        ;; Category
+        (:startgroup)
+        ("@work" . ?w)
+        (:grouptags)
+        ("@caracal" . ?c)
+        ("@dlt" . ?d)
+        (:endgroup)
+
+        ;; Projects
+        (:startgrouptag)
+        ("@project" . ?p)
+        (:endgrouptag)
+
+
+        ;; Emacs
+        (:startgrouptag)
+        ("@emacs" . ?e)
+        (:endgrouptag)
+
+        ;; AI
+        (:startgrouptag)
+        ("@ai" . ?a)
+        (:grouptags)
+        ("@llm" . ?c)
+        (:endgrouptag)
+
+        ;; Misc
+        (:startgrouptag)
+        ("@ref" . ?r)                   ;; Reference material
+        (:endgrouptag)))
