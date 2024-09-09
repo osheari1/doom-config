46c8c9cc4281aefb91b84ae956865492b0cc7917
Riley O'Shea <osheari1@gmail.com>
2024-09-09 12:55:08 +0200

    feat: add todo dependencies enforcement in org-mode

--- confs/org-mode.el
+++ confs/org-mode.el
@@ -3,4 +3,7 @@
 
 (setq org-agenda-files (directory-files-recursively org-directory "\\.org$")
       org-log-done 'time ;; Adds a timestamp and/or note upon task completion
-      org-complete-tags-always-offer-all-agenda-tags t)
+      org-complete-tags-always-offer-all-agenda-tags t
+
+      org-enforce-todo-dependencies t
+      org-enforce-todo-checkbox-dependencies t)
