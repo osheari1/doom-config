59c7557bf6f3610b0c3a0b0525a237d2b410c837
Riley O'Shea <osheari1@gmail.com>
2024-09-10 11:46:42 +0200

    feat: improve gptel directives for programming and org-mode

--- confs/gptel.el
+++ confs/gptel.el
@@ -21,9 +21,10 @@
 
 (setq gptel-directives
       '((default . "")
-        (programming . "You are a large language model and a careful programmer. Provide code and only code as output without any additional text, prompt or note. No code fences.")
+        (programming . "You are a large language model and a careful programmer. Provide code and only code as output without any additional text, prompt or note. Do not wrap in a code fence (eg ```emacs-lisp ... ```)")
         (emacs . "Act as an emacs expert. Respond concisely. Where examples would support the answer, please add them.")
-        (emacs-code . "You are an expert on emacs and an expert emacs lisp programmer. Provide code and only code as output without any additional text, prompt or note. Do not wrap in a markdown codeblock (eg ```emacs-lisp ... ```)")
+        (elisp . "You are an expert on emacs and an expert emacs lisp programmer. Provide code and only code as output without any additional text, prompt or note. Do not wrap in a markdown codeblock (eg ```emacs-lisp ... ```)")
+        (org-mode . "You are an expert on emacs and an emacs org-mode writer. Provide org-mode formatted text only without any additional text, prompt or note. Do not wrap in a markdown codeblock (eg ```emacs-lisp ... ```)")
         (dlt . "Act as an expert on Distributed Ledger Technologies (DLT), blockchain, and crypto. Respond concisely. Where examples would support the answer, please add them.")
         )
       gptel-prompt-prefix-alist
