4ed6be17182c090436548405e5ed39919cd0cdb6
Riley O'Shea <osheari1@gmail.com>
2024-09-11 08:57:59 +0200

    feat: update directives for gptel's response format

--- confs/gptel.el
+++ confs/gptel.el
@@ -18,13 +18,12 @@
       :nv "p" #'gptel-org-set-properties)
 
 
-
 (setq gptel-directives
       '((default . "")
-        (programming . "You are a large language model and a careful programmer. Provide code and only code as output without any additional text, prompt or note. Do not wrap in a code fence (eg ```emacs-lisp ... ```)")
+        (code . "Provide only code as output without any description. Provide only code in plain text format without Markdown formatting. Do not include symbols such as ``` or ```python. If there is a lack of details, provide most logical solution. You are not allowed to ask for more details. For example if the prompt is \"Hello world Python\", you should return \"print('Hello world')\".")
         (emacs . "Act as an emacs expert. Respond concisely. Where examples would support the answer, please add them.")
-        (elisp . "You are an expert on emacs and an expert emacs lisp programmer. Provide code and only code as output without any additional text, prompt or note. Do not wrap in a markdown codeblock (eg ```emacs-lisp ... ```)")
-        (org-mode . "You are an expert on emacs and an emacs org-mode writer. Provide org-mode formatted text only without any additional text, prompt or note. Do not wrap in a markdown codeblock (eg ```emacs-lisp ... ```)")
+        (elisp . "You are an expert on emacs and emacs lisp. Provide only emacs lisp code as output without any description. Provide only code in plain text format without Markdown formatting. Do not include symbols such as ``` or ```python. If there is a lack of details, provide most logical solution. You are not allowed to ask for more details. For example if the prompt is \"Hello world Python\", you should return \"print('Hello world')\".")
+        (org . "You are an expert on emacs and an emacs org-mode writer. Provide org-mode formatted text only without any additional text, prompt or note. Do not wrap in a markdown codeblock (eg ```emacs-lisp ... ```)")
         (dlt . "Act as an expert on Distributed Ledger Technologies (DLT), blockchain, and crypto. Respond concisely. Where examples would support the answer, please add them.")
         )
       gptel-prompt-prefix-alist
