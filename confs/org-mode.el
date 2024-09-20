ad7a0533a3607654011f2561c1144702640523bf
Riley O'Shea <osheari1@gmail.com>
2024-09-20 15:24:25 +0200

    refactor: change org-log-done setting for task completion

--- confs/org-mode.el
+++ confs/org-mode.el
@@ -2,7 +2,7 @@
 
 
 (setq org-agenda-files (directory-files-recursively org-directory "\\.org$")
-      org-log-done 'time ;; Adds a timestamp and/or note upon task completion
+      org-log-done 'note ;; Adds a timestamp and/or note upon task completion
       org-complete-tags-always-offer-all-agenda-tags t
 
       org-enforce-todo-dependencies t
