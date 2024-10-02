b126c861c7d64542d0edba98ffe582fcc17e11ef
Riley O'Shea <osheari1@gmail.com>
2024-10-02 14:23:22 +0200

    feat: Refactor telega configuration for improved readability

--- confs/telega.el
+++ confs/telega.el
@@ -1,7 +1,10 @@
 ;;; confs/telega.el -*- lexical-binding: t; -*-
 
-(setq telega-server-libs-prefix "/usr"
-      telega-accounts (list
-                       (list "Riley" 'telega-database-dir telega-database-dir)
-                       (list "Riley W" 'telega-database-dir
-                             (expand-file-name "w" telega-database-dir))))
+
+(use-package! telega
+  :config
+  (setq telega-server-libs-prefix "/usr"
+        telega-accounts (list
+                         (list "Riley" 'telega-database-dir telega-database-dir)
+                         (list "Riley W" 'telega-database-dir
+                               (expand-file-name "w" telega-database-dir)))))
