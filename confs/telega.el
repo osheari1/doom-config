48b950db5b8224c0c05ad70b3d418f8ef670fde1
Riley O'Shea <osheari1@gmail.com>
2024-10-08 19:52:40 +0100

    refactor: simplify telega.el account list structure

--- confs/telega.el
+++ confs/telega.el
@@ -7,8 +7,4 @@
         telega-accounts (list
                          (list "Riley" 'telega-database-dir telega-database-dir)
                          (list "Riley W" 'telega-database-dir
-                               (expand-file-name "w" telega-database-dir))))
-
-  (evil-set-initial-state 'telega-root-mode 'emacs)
-  (evil-set-initial-state 'telega-chat-mode 'emacs)
-  (evil-set-initial-state 'telega-image-mode 'emacs))
+                               (expand-file-name "w" telega-database-dir)))))
